e13166cb59af7e9a37355292ff0a0ff9d91e57c4
Tyler Johnston <tylerjohnst@gmail.com>
2018-12-02 13:59:14 -0500

    Initial commit

---
+++ generate.rb
@@ -0,0 +1,45 @@
+require 'yaml'
+require 'google/apis/sheets_v4'
+require 'csv'
+require 'prawn/labels'
+
+Prawn::Labels.types = './avery.yml'
+
+SECRETS = YAML.load(File.read(File.join(__dir__, 'secrets.yml')))
+SHEET_ID = SECRETS['sheet_id']
+
+$service = Google::Apis::SheetsV4::SheetsService.new
+$service.key = SECRETS['google_key']
+
+#
+# Food
+#
+
+$food = $service.get_spreadsheet_values(SHEET_ID, 'Food').values[1..-1].map do |row|
+  count, *rest = row
+  Array.new(count.to_i, rest)
+end.flatten(1)
+
+Prawn::Labels.generate(File.expand_path("~/Desktop/food.pdf"), $food, type: 'Avery5160') do |pdf, row|
+  name, carbs, protein, fat, calories = row
+  pdf.font File.join(__dir__, '/SignPainter.ttf') do
+    pdf.text "Low Carb Lunch Lady", align: :center, size: 14
+  end
+
+  pdf.text name + "\n" +
+           "#{calories}: #{fat}g/#{carbs}g/#{protein}g",
+           align: :center
+end
+
+#
+# Customers
+#
+
+$customers = $service.get_spreadsheet_values(SHEET_ID, 'customers').values[1..-1].map do |row|
+  count, *rest = row
+  Array.new(count.to_i, rest)
+end.flatten(1)
+
+Prawn::Labels.generate(File.expand_path("~/Desktop/customers.pdf"), $customers, type: 'Avery8167') do |pdf, row|
+  pdf.text row.reverse.join(', '), align: :center, style: :bold
+end
